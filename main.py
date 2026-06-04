aa9976958d116c41a96057347c6f176ccef07798
ToneRiper <tongtong945app@gmail.com>
2026-06-04 17:50:05 +0800

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import os
 import requests
-import feedparser
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-rss_url = "https://www.gov.cn/zhengce/zuixin/home.htm"
+url = "https://www.gov.cn"
 
-message = "【A股AI超级雷达】\n\n"
+msg = "【A股AI超级雷达】\n\n"
 
 try:
-    response = requests.get(rss_url, timeout=20)
+    r = requests.get(
+        url,
+        timeout=20,
+        headers={
+            "User-Agent": "Mozilla/5.0"
+        }
+    )
 
-    if response.status_code == 200:
-        message += "国务院政策页面访问成功\n"
-        message += f"状态码：{response.status_code}\n"
-    else:
-        message += "国务院页面访问失败\n"
+    msg += f"状态码: {r.status_code}\n"
+    msg += f"页面长度: {len(r.text)}\n"
 
 except Exception as e:
-    message += f"错误：{str(e)}\n"
-
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    msg += f"错误:\n{str(e)}"
 
 requests.post(
-    url,
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": msg
     }
 )
